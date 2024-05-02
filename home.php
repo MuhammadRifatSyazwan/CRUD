bbc6b06c7e0910969ac64c79c559fc4e7c662a39
MuhammadRifatSyazwan <rifathakim315@gmail.com>
2024-05-02 14:29:34 +0700

    upload <b>Aplikasi CRUD</b>

---
+++ home.php
@@ -0,0 +1,7 @@
+<div class="card">
+    <div class="card-body">
+        <img src="images/0.png" alt="TKJ" class="float-end" height="200">
+        <br>
+        RGI mengadopsi platform pondok pesantren, fokus pada penyelenggaraan pendidikan non formal dalam kemasan short course (kursus singkat). Perpaduan ini agar para peserta pelatihan RGI tidak hanya memiliki keterampilan unggul yang menjadi pondasi masa depan, tapi juga memiliki pengetahuan dan dasar akidah, iman, serta akhlak yang baik.
+    </div>
+</div>
